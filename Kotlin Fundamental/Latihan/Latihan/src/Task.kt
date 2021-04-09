efea75da39788a022ee8491b77d019330f1cadd6
Hadinata Salim <54607817+hadinatasalim100@users.noreply.github.com>
2021-04-09 07:40:38 +0700

    this code

---
+++ Kotlin Fundamental/Latihan/Latihan/src/Task.kt
@@ -0,0 +1,28 @@
+/**
+ * Untuk menyelesaikan tugas latihan, Anda tidak diperbolehkan mengubah struktur kode yang sudah ada. Kecuali:
+ *    - Untuk melakukan improvisasi kode
+ *    - Mengikuti perintah yang ada
+ *
+ * Cukup tambahkan kode berdasarkan perintah yang sudah ditentukan.
+ *
+ */
+
+fun main() {
+    val valueA = 101
+    val valueB = 52
+    val valueC = 99
+
+    val resultA = valueB + (valueA - valueC)
+    val resultB = valueA + (valueA - valueC)
+
+    println("""
+        ResultA is $resultA
+        ResultB is $resultB
+    """.trimIndent())
+}
+
+fun calculateResult(valueA: Int, valueB: Int, valueC: Int?): Int {
+    // TODO
+    val result = 0
+    return result
+}
